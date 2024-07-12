9be62eab7aca93a6f903dbad589d16e41bb1577e
Brijesh Dubey <bkd101994@gmail.com>
2024-07-12 21:07:18 +0530

    Firebase base setup

---
+++ app/src/main/java/com/example/notification/firebaseHelper/MyFirebaseMessagingService.kt
@@ -0,0 +1,29 @@
+package com.example.notification.firebaseHelper
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    private val TAG = MyFirebaseMessagingService::class.java.name
+
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // FCM registration token to your app server.
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+        }
+
+        // Check if message contains a notification payload.
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+        }
+    }
+
+}
